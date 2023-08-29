37118a1233eb33fe7846f8adeddd31e9b8826d79
Onur Ozkinaci <onurozkinaci@gmail.com>
2023-08-29 17:30:24 +0300

    AdminPanels Blog and Comment sections are updated.

--- TravelTripProject/Controllers/AdminController.cs
+++ TravelTripProject/Controllers/AdminController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI;
 using TravelTripProject.Models.Classes;
 
 namespace TravelTripProject.Controllers
@@ -11,10 +13,73 @@ namespace TravelTripProject.Controllers
     {
         // GET: Admin
         Context context = new Context();
-        public ActionResult Index()
+
+        //Pagination control is applied before sending the data to view in order to show max 10 data in each page by default;
+        public ActionResult Index(int page = 1, int pageSize = 10)
         {
-            var values = context.Blogs.ToList();
-            return View(values);
+            List<Blog> allBlogs = context.Blogs.ToList();
+            int totalBlogs = allBlogs.Count;
+            List<Blog> paginatedBlogs = allBlogs
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+            ViewBag.TotalBlogs = totalBlogs;
+            ViewBag.CurrentPage = page;
+            ViewBag.PageSize = pageSize;
+            return View(paginatedBlogs);
+        }
+        #region AddNewBlog
+        [HttpGet]
+        public ActionResult NewBlog()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult NewBlog(Blog newBlog)
+        {
+            context.Blogs.Add(newBlog);
+            context.SaveChanges();
+            return RedirectToAction("Index"); //Index action'ina yonlendirip eklenen blogu listeleyecek.
+        }
+        #endregion 
+
+        public ActionResult DeleteBlog(int id) 
+        {
+            var blog = context.Blogs.Find(id); //found blog by it's Primary Key
+            context.Blogs.Remove(blog);
+            context.SaveChanges();
+            return RedirectToAction("Index"); //Index action'ina yonlendirip silinen blogu listeleyecek.
+        }
+
+        #region UpdatingTheBlog
+        public ActionResult FindBlog(int id)
+        {
+            var blog = context.Blogs.Find(id); //found blog
+            return View("FindBlog", blog);
+        }
+        public ActionResult BlogUpdate(Blog updatedBlog)
+        {
+            #region Not
+             /*'Find()' ile bulunan nesne context'e attach edilir ve bu nesne uzerinde yaptigin degisiklikler context
+             tarafindan izlenip(tracked) direkt context'i etkiler. Boylece, nesneye ait propertylerin yeni 
+             degerlerini atadiktan sonra direkt olarak `context.SaveChanges();` kullanimi ile Db uzerindeki 
+             guncellemeyi de saglayabilirsin;*/
+            #endregion
+            var blog = context.Blogs.Find(updatedBlog.ID); //the blog which is going to be updated is found.
+            blog.Description = updatedBlog.Description;
+            blog.Title = updatedBlog.Title;
+            blog.BlogImage = updatedBlog.BlogImage;
+            blog.Date = updatedBlog.Date;
+            context.SaveChanges();
+            return RedirectToAction("Index"); //Index action'ina yonlendirip guncellenen blogu listeleyecek.
+        }
+        #endregion
+
+        public ActionResult CommentList()
+        {
+            var comments = context.Yorumlars.ToList();
+            return View(comments);
         }
     }
 }
--- TravelTripProject/Controllers/BlogController.cs
+++ TravelTripProject/Controllers/BlogController.cs
@@ -17,14 +17,14 @@ namespace TravelTripProject.Controllers
         {
             //var blogs = context.Blogs.ToList(); //var blogs yerine List<Blog> da kullanilabilir.
             blogYorum.Deger1 = context.Blogs.ToList();
-            blogYorum.Deger3 = context.Blogs.OrderByDescending(x=>x.ID).Take(3).ToList(); //son 3 blogu dondurur.
+            blogYorum.Deger3 = context.Blogs.OrderByDescending(x => x.ID).Take(3).ToList(); //son 3 blogu dondurur.
             //------
             blogYorum.Deger4 = context.Yorumlars.OrderByDescending(x => x.ID).Take(3).ToList(); //son 3 yorumu dondurur.
             //------
             return View(blogYorum);
         }
 
-        
+
         public ActionResult BlogDetail(int id)
         {
             //var foundBlog = context.Blogs.Where(x => x.ID == id).ToList(); //finding the matched blog with LINQ query.
@@ -36,5 +36,26 @@ namespace TravelTripProject.Controllers
             //----------
             return View(blogYorum);
         }
+
+        #region AddComment
+        #region Not
+        /* BlogDetail(int id) action metodu cagrildiginda iletilen 'id' parametresi ayni isimle 'MakeComment()' partial view metoduna verildiginde de erisilebilirdir cunku MakeComment partial view'i bu BlogDetail view'i icerisinde cagrilmaktadir. Ayni url context'inde bulundugundan, url uzerinden iletilen 'id' degerine bu view icerisinde cagrilan diger partial viewler de parametre adini ayni sekilde kullanarak erisebilir konumdadir.Bu baglamda, BlogDetail(int id) metodu cagrildiginda iletilmis olan ve id parametresine atanan blog id'si, bu metodun 
+          dondurdugu view icerisinde cagrilan partial view metodu ('MakeComment()') icin de erisilebilir olacaktir.*/
+        #endregion
+        [HttpGet]
+        public PartialViewResult MakeComment(int id)
+        {
+            ViewBag.value = id; //Controller'dan view'e veri tasimak icin kullanildi.
+            return PartialView();
+        }
+
+        [HttpPost]
+        public PartialViewResult MakeComment(Yorumlar yorum)
+        {
+            context.Yorumlars.Add(yorum);
+            context.SaveChanges();
+            return PartialView();
+        }
+        #endregion
     }
 }
