5522e48877125c5f7ada1a33578a26a476c246b1
Onur Ozkinaci <onurozkinaci@gmail.com>
2023-09-01 19:13:39 +0300

    Admin Panel is updated and authentication is added.

--- TravelTripProject/Controllers/AdminController.cs
+++ TravelTripProject/Controllers/AdminController.cs
@@ -9,11 +9,14 @@ using TravelTripProject.Models.Classes;
 
 namespace TravelTripProject.Controllers
 {
+    [Authorize] //Bu controllerda tanimlanan tum viewlere(action result uzerinden) erisime yetki istemek icin.
     public class AdminController : Controller
     {
         // GET: Admin
         Context context = new Context();
 
+
+        //[Authorize] => yalnizca Index() ActionResult metodundan donen view ozelinde erisime yetki istemek icin.
         //Pagination control is applied before sending the data to view in order to show max 10 data in each page by default;
         public ActionResult Index(int page = 1, int pageSize = 10)
         {
@@ -28,7 +31,7 @@ namespace TravelTripProject.Controllers
             ViewBag.PageSize = pageSize;
             return View(paginatedBlogs);
         }
-        #region AddNewBlog
+        #region Add New Blog
         [HttpGet]
         public ActionResult NewBlog()
         {
@@ -44,7 +47,7 @@ namespace TravelTripProject.Controllers
         }
         #endregion 
 
-        public ActionResult DeleteBlog(int id) 
+        public ActionResult DeleteBlog(int id)
         {
             var blog = context.Blogs.Find(id); //found blog by it's Primary Key
             context.Blogs.Remove(blog);
@@ -52,7 +55,7 @@ namespace TravelTripProject.Controllers
             return RedirectToAction("Index"); //Index action'ina yonlendirip silinen blogu listeleyecek.
         }
 
-        #region UpdatingTheBlog
+        #region Updating The Blog
         public ActionResult FindBlog(int id)
         {
             var blog = context.Blogs.Find(id); //found blog
@@ -61,10 +64,10 @@ namespace TravelTripProject.Controllers
         public ActionResult BlogUpdate(Blog updatedBlog)
         {
             #region Not
-             /*'Find()' ile bulunan nesne context'e attach edilir ve bu nesne uzerinde yaptigin degisiklikler context
-             tarafindan izlenip(tracked) direkt context'i etkiler. Boylece, nesneye ait propertylerin yeni 
-             degerlerini atadiktan sonra direkt olarak `context.SaveChanges();` kullanimi ile Db uzerindeki 
-             guncellemeyi de saglayabilirsin;*/
+            /*'Find()' ile bulunan nesne context'e attach edilir ve bu nesne uzerinde yaptigin degisiklikler context
+            tarafindan izlenip(tracked) direkt context'i etkiler. Boylece, nesneye ait propertylerin yeni 
+            degerlerini atadiktan sonra direkt olarak `context.SaveChanges();` kullanimi ile Db uzerindeki 
+            guncellemeyi de saglayabilirsin;*/
             #endregion
             var blog = context.Blogs.Find(updatedBlog.ID); //the blog which is going to be updated is found.
             blog.Description = updatedBlog.Description;
@@ -81,5 +84,31 @@ namespace TravelTripProject.Controllers
             var comments = context.Yorumlars.ToList();
             return View(comments);
         }
+
+        public ActionResult DeleteComment(int id)
+        {
+            var comment = context.Yorumlars.Find(id); //found comment
+            context.Yorumlars.Remove(comment);
+            context.SaveChanges();
+            return RedirectToAction("CommentList"); //ilgili yorumu sildikten sonra yorumlar listelenecek.
+        }
+
+        #region Updating The Comment
+        public ActionResult FindComment(int id)
+        {
+            var comment = context.Yorumlars.Find(id); //found comment
+            return View("FindComment", comment); //direkt "return View(comment);" de calisir.
+        }
+        public ActionResult UpdateComment(Yorumlar updatedComment)
+        {
+            var comment = context.Yorumlars.Find(updatedComment.ID);
+            comment.Username = updatedComment.Username;
+            comment.Mail = updatedComment.Mail;
+            comment.Comment = updatedComment.Comment;
+            //comment.BlogId = updatedComment.BlogId;
+            context.SaveChanges();
+            return RedirectToAction("CommentList"); //ilgili yorumu guncelledikten sonra yorumlar listelenecek.
+        }
+        #endregion
     }
 }
---
+++ TravelTripProject/Controllers/LoginController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+using System.Web.Security;
+using TravelTripProject.Models.Classes;
+
+namespace TravelTripProject.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        Context context = new Context();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        #region Login Islemleri
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Admin admin)
+        {
+            var infos = context.Admins.FirstOrDefault(x => x.Username == admin.Username && x.Password == admin.Password);
+            if (infos != null)
+            {
+                FormsAuthentication.SetAuthCookie(infos.Username, false); //cerez atamasi yapilir.
+                Session["Kullanici"] = infos.Username.ToString(); //login olan kullanici icin session atamasi yapilir.
+                return RedirectToAction("Index", "Admin");
+            }
+            else
+                return View();
+        }
+        #endregion
+        
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut(); //removes the form-authentication ticket from the browser which is created when the user logs in with the usage of "SetAuthCookie()" at the above 'Login()' function.
+            return RedirectToAction("Login","Login"); //tekrar login sayfasina yonlendirir.
+        }
+    }
+}
